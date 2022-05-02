03d9d167b2f7af0005dd53772a426004cdefcb11
Marina <lazarevic.ui@gmail.com>
2022-05-03 00:52:24 +0200

    basic page styling

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,15 @@ export default {
 </script>
 
 <style lang="scss">
+@use '@/scss/abstract' as a;
 
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    background-color: a.$dark;
+}
 </style>
